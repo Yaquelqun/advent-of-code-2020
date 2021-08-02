f8b2463f653b51a7b3cd028703505173fef05462
Yaquelqun <sandjiv.parassou@gmail.com>
2021-08-02 11:18:41 +0200

    solve part 2 of day 5

--- lib/solvers/plane_seat_parser.rb
+++ lib/solvers/plane_seat_parser.rb
@@ -7,16 +7,27 @@ module AdventOfCode2020
 
     def solve
       puts "part1: #{max_seat_id}"
-      # puts "part2: #{count_valid_passwords(strategy: "presence")}"
+      puts "part2: #{my_seat_id}"
     end
 
     private
 
     def max_seat_id
-      ::Connectors::AocConnector.new(endpoint: "day_5_input")
-                                .parse_data
-                                .map { |seat_description| Seat.new(seat_description) }
-                                .reduce(0) { |max, seat| [max, seat.id].max }
+      sorted_seat_ids.last
+    end
+
+    def my_seat_id
+      ids_range = (sorted_seat_ids.first..sorted_seat_ids.last)
+      (ids_range.to_a - sorted_seat_ids).first 
+    end
+
+    def sorted_seat_ids
+      @sorted_seat_ids ||= ::Connectors::AocConnector.new(endpoint: "day_5_input")
+                                              .parse_data
+                                              .map { |seat_description| Seat.new(seat_description) }
+                                              .map(&:id)
+                                              .sort
+
     end
   end
 end
