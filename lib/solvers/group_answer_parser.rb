bf9ab6a2608bf419a696d012a0a26d6c3126f70e
Yaquelqun <sandjiv.parassou@gmail.com>
2021-08-03 08:59:44 +0200

    solve part 2 (that i actually did first... and refactor parser)

--- lib/solvers/group_answer_parser.rb
+++ lib/solvers/group_answer_parser.rb
@@ -4,20 +4,37 @@ require_relative "../models/group_answer"
 
 class GroupAnswerParser
   def solve
-    puts "part 1: #{group_common_positive_answer_counts.reduce(&:+)}"
-    # puts "part 2: #{}"
+    puts "part 1: #{total_positive_answer_sum}"
+    puts "part 2: #{common_positive_answer_sum}"
   end
 
   private
 
-  def total_positive_answer_counts
-    parsed_input = ::Connectors::AocConnector.new(endpoint: "day_6_input")
-                                             .parse_data
+  def total_positive_answer_sum
+    total_positive_answers.map(&:count).reduce(:+)
+  end
 
-    ::GroupAnswers::GroupAnswerAggregator.new(parsed_input)
-                                         .aggregate
-                                         .map { |group_positive_answers| GroupAnswer.new(group_positive_answers) }
-                                         .map(&:total_positive_answers)
-                                         .map(&:count)
+  def common_positive_answer_sum
+    common_positive_answers.map(&:count).reduce(:+)
+  end
+
+  def parsed_input
+    @parsed_input ||= ::Connectors::AocConnector.new(endpoint: "day_6_input")
+                                                .parse_data
+  end
+
+  def group_answers
+    @group_answers ||= ::GroupAnswers::GroupAnswerAggregator
+      .new(parsed_input)
+      .aggregate
+      .map { |group_positive_answers| GroupAnswer.new(group_positive_answers) }
+  end
+
+  def total_positive_answers
+    group_answers.map(&:total_positive_answers)
+  end
+
+  def common_positive_answers
+    group_answers.map(&:common_positive_answers)
   end
 end
